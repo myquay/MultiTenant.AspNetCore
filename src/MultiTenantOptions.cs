41f2a27279119b614b1ea1178a1bd7f73ae3587f
Michael McKenna <michael@solve.solutions>
2024-05-04 21:55:07 +1200

    Ability to manually specify middleware ordering

---
+++ src/MultiTenantOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenant.AspNetCore
+{
+    /// <summary>
+    /// Configuration options
+    /// </summary>
+    public class MultiTenantOptions
+    {
+        /// <summary>
+        /// Disable the automatic registration of the multitenant pipeline
+        /// </summary>
+        public bool DisableAutomaticPipelineRegistration { get; set; } = false;
+    }
+}
--- src/Registration/WebBuilderExtensions.cs
+++ src/Registration/WebBuilderExtensions.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using MultiTenant.AspNetCore.Builder;
 using MultiTenant.AspNetCore.Infrastructure.Middleware;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MultiTenant.AspNetCore
 {
@@ -16,15 +18,40 @@ namespace MultiTenant.AspNetCore
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
-        public static TenantBuilder<T> AddMultiTenancy<T>(this IServiceCollection Services) where T : ITenantInfo
+        public static TenantBuilder<T> AddMultiTenancy<T>(this IServiceCollection Services, Action<MultiTenantOptions>? configure = null) where T : ITenantInfo
         {
+            //Register the options
+            var options = new MultiTenantOptions();
+            configure?.Invoke(options);
+            Services.Configure<MultiTenantOptions>(o => configure?.Invoke(o));
+
+
             //Provide ambient tenant context
             Services.TryAddSingleton<IMultiTenantContextAccessor<T>, AsyncLocalMultiTenantContextAccessor<T>>();
 
             //Register middleware to populate the ambient tenant context early in the pipeline
-            Services.Insert(0, ServiceDescriptor.Transient<IStartupFilter>(provider => new MultiTenantContextAccessorStartupFilter<T>()));
+            if (!options.DisableAutomaticPipelineRegistration)
+                Services.Insert(0, ServiceDescriptor.Transient<IStartupFilter>(provider => new MultiTenantContextAccessorStartupFilter<T>()));
 
-            return new TenantBuilder<T>(Services);
+            return new TenantBuilder<T>(Services, options);
+        }
+
+        //Register the multitenant request services middleware manually for more control over operational ordering
+        public static IApplicationBuilder UseMultiTenancy<T>(this IApplicationBuilder builder)
+            where T : ITenantInfo
+        {
+            //Check if the startup registration is disabled
+            var services = builder.ApplicationServices.GetServices<IStartupFilter>()
+                .Where(s => s.GetType().IsGenericType);
+
+            if (services.Any(s => s.GetType().GetGenericTypeDefinition() == typeof(MultitenantRequestServicesStartupFilter<>)))
+                throw new InvalidOperationException("UseMultiTenant must only be called if startup registration is disabled, set 'DisableAutomaticPipelineRegistration' to true");
+            if (services.Any(s => s.GetType().GetGenericTypeDefinition() == typeof(MultiTenantContextAccessorStartupFilter<>)))
+                throw new InvalidOperationException("UseMultiTenant must only be called if startup registration is disabled, set 'DisableAutomaticPipelineRegistration' to true");
+
+            //Register the multitenant request services middleware with the app pipeline
+            return builder.UseMiddleware<MultiTenantContextAccessorMiddleware<T>>()
+                .UseMiddleware<MultiTenantRequestServicesMiddleware<T>>();
         }
     }
 }
